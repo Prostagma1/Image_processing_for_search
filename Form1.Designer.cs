2a92be80c4850842ca48fd08559ae96d50d7c4d3
Prostagma <dlinchaki@mail.ru>
2023-05-09 19:56:14 +0300

    добавлена возможность отображать фотографиииз выбранной рабочей папки

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(575, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(569, 435);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 109);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(168, 84);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 66);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Выбрать папку с фото";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1189, 621);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Линчак ЛР3";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,49 @@ namespace LABA3
 {
     public partial class Form1 : Form
     {
+        List<string> paths = new List<string> { };
+        Bitmap currentBitmap;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.SelectedPath = @"D:\Study\4 sem\TechnicalVision\RoadSignsDataSet";
+
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+                paths.Clear();
+
+                string[] files = Directory.GetFiles(folder.SelectedPath);
+
+                foreach (var file in files)
+                {
+                    var ext = Path.GetExtension(file);
+
+                    if (ext == ".bmp" || ext == ".png" || ext == ".jpg")
+                    {
+                        paths.Add(file);
+                        listBox1.Items.Add(Path.GetFileName(file));
+                    }
+                }
+            }
+            folder.Dispose();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                currentBitmap = new Bitmap(paths[listBox1.SelectedIndex]);
+                pictureBox1.Height = currentBitmap.Height;
+                pictureBox1.Width = currentBitmap.Width;
+                pictureBox1.Image = currentBitmap;
+            }
+
         }
     }
 }
